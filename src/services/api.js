62380ed71215048e589126b9a938bea2b7469069
matheus.goncalves <matheusvieira1900@hotmail.com>
2020-06-21 10:31:39 -0300

    Adicionando variavel ambiente para URL da API

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-   baseURL: "https://chronic-disease-control-back.herokuapp.com",
-   // baseURL: "http://localhost:3333",
+   baseURL: process.env.REACT_APP_API_URL,
 });
 
 export default api;
